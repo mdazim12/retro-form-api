3dfb1565f0a698e3869f00a868ea365f2a30b91e
Azim <azimuddin807@gmail.com>
2024-10-04 22:18:37 +0600

    dynamic search api

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
     const loadAllPost = async(category) => {
-        
-        if(category){
-            console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`)
-        }
-        else{
-            console.log('https://openapi.programming-hero.com/api/retro-forum/posts')
-        }
 
-        // const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
-        // const data = await response.json();
-        // console.log(data.posts)
+        // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category?`?category=${category}` : ''}`);
+        
+        // if(category){
+        //     console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`)
+        // }
+        // else{
+        //     console.log('https://openapi.programming-hero.com/api/retro-forum/posts')
+        // }
+
+        const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category?`?category=${category}` : ''}`);
+        const data = await response.json();
+        console.log(data.posts)
     }
 
     loadAllPost();
