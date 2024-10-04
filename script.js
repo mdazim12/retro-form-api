c00e6e8b7d18c60f6a93b8e5759fe6d520e37529
Azim <azimuddin807@gmail.com>
2024-10-04 21:55:24 +0600

    add search text on api query

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    const loadAllPost = async(category) => {
+        
+        if(category){
+            console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`)
+        }
+        else{
+            console.log('https://openapi.programming-hero.com/api/retro-forum/posts')
+        }
+
+        // const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+        // const data = await response.json();
+        // console.log(data.posts)
+    }
+
+    loadAllPost();
+
+    const seacrhByCategory = () => {
+        const searhText = document.getElementById('searchPosts').value;
+        loadAllPost(searhText)
+    }
